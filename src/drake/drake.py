ebf19ea20c706e2c063715f40c5f5f50b93cf494
Adrian Vecina Tercero <psyav2@nottingham.ac.uk>
2022-09-30 21:49:24 +0100

    Continued Refactoring to prepare for change to FCOS

--- src/drake/drake.py
+++ src/drake/drake.py
@@ -1,5 +1,6 @@
 import cv_bridge
 import intel_extension_for_pytorch as ipex
+from fcos.predict import prediction
 import rospy
 import torch
 from drake.msg import DrakeResults, DrakeResult
@@ -14,7 +15,7 @@ class Drake:
     def __init__(self, /, size, confidence, iou, classes, exportImage, image):
         # # Initialise the Model
 
-        model = torch.load('./models/net50.pkl')
+        model = torch.load('./models/net50.pkl') # Will currently only be object recognition model
         model.conf = confidence  # confidence threshold (0-1)
         model.iou = iou  # NMS IoU threshold (0-1)
         model.classes = classes if len(
@@ -31,35 +32,41 @@ class Drake:
         self.bridge = cv_bridge.CvBridge()
 
         # ## Setup publishers
-        self.publishers = dict()
-        self.publishers["image_with_bounding_boxes"] = rospy.Publisher('drake/image_with_bounding_boxes', Image,
-                                                                       queue_size=1)
-        self.publishers["bounding_boxes"] = rospy.Publisher('/drake/bounding_boxes', DrakeResults)
-
+        self.publishers = {
+            "image_with_bounding_boxes": rospy.Publisher('drake/image_with_bounding_boxes', Image, queue_size=1),
+            "bounding_boxes": rospy.Publisher('/drake/bounding_boxes', DrakeResults)
+        }
         # ## Setup subscribers
-        self.subscribers = dict()
-        self.subscribers["image"] = rospy.Subscriber(image, Image, self._onImageReceived)
-        self.currentData = None
-
+        self.subscribers = {
+            "image" : rospy.Subscriber(image, Image, self._onImageReceived)
+        }
+        
+        self.currentImage = None
         self.exportImage = exportImage
 
-    def _onImageReceived(self, ros_data):
-        self.currentData = ros_data
+    # When we get an Image msg
+    def _onImageReceived(self, msg):
+        self.currentImage = msg
 
+    # Publishes bounding boxes and images to 
     def _publishImage(self):
-        data = self.currentData
-        if (data is None):
+        data = self.currentImage
+        if (data is None): 
+            # No image.
             rospy.logwarn("No Image to publish...")
             return
-        image = self.bridge.imgmsg_to_cv2(data, desired_encoding='bgr8')
+        
+        image = self.bridge.imgmsg_to_cv2(data, desired_encoding='bgr8') # Makes the ROS image work with 
 
+        output = DrakeResults()
+        
         results = self.model(image)
         rospy.loginfo(results)
 
         xyxy = results.pandas().xyxy[0]
         xyxy.rename(columns={"class": "class_"}, inplace=True)
 
-        output = DrakeResults()
+        
         output.speed, output.inference, output.NMSPerImage = results.t
         output.results = [DrakeResult(**detection) for detection in xyxy.to_dict(orient='records')]
         output.resultsCount = len(results)
